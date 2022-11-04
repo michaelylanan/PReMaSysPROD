7b603106362ece5170417c493aa8f8ad79836503
michaelylanan <57473006+michaelylanan@users.noreply.github.com>
2022-11-04 18:51:07 +0800

    Update ManageNavPages.cs

--- PReMaSys/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ PReMaSys/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -3,7 +3,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace  Testertest.Areas.Identity.Pages.Account.Manage
+namespace  PReMaSys.Areas.Identity.Pages.Account.Manage
 {
 
     public static class ManageNavPages

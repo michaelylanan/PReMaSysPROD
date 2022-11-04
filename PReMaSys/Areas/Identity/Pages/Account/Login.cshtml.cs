fbfd6ae6408d4bf6b35b992f7f57284017f010d1
michaelylanan <57473006+michaelylanan@users.noreply.github.com>
2022-11-04 18:42:00 +0800

    Update Login.cshtml.cs
    Fixed Correct Name Conventions

--- PReMaSys/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PReMaSys/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using Testertest.Data;
+using PReMaSys.Data;
 
-namespace Testertest.Areas.Identity.Pages.Account
+namespace PReMaSys.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {

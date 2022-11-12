db83f61eddcf627bcc1c58360f2f8c9ee14acc8d
Kevin <nicoleann.cepria@gmail.com>
2022-11-12 12:51:47 +0800

    Continue email verification Fe

---
+++ PReMaSys/Controllers/EmailNotifController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PReMaSys.Controllers
+{
+    public class EmailNotifController : Controller
+    {
+
+        public IActionResult email()
+        {
+            return View();
+        }
+    }
+}
